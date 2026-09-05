3df4e41272e5be7f4ec721c145ce901e8d1ad18d
el-hoffer <lhoffer@zscaler.com>
2026-09-05 15:12:17 -0500

    list_gateways typo

--- upgrader/upgrader.py
+++ upgrader/upgrader.py
@@ -72,7 +72,7 @@ class APIClient:
         """Return gateway records from the API."""
         data_ztb = self._request("GET", "v3/Gateway/")
         data_hub = self._request("GET", "v3/Gateway/?gateway_type=access")
-        gateways = data_ztb["result"]["rows"] + data_hub["result"]["rows"]
+        gateways = data_ztb["result"]["rows"]["gateways"] + data_hub["result"]["rows"]["gateways"]
         return [
             gateway
             for gateway in gateways
